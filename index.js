06ca04daedc76d73f3a198b1aafe52d701768ee4
brittany bellantoni <brittt2121@hotmail.com>
2017-11-20 01:36:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function longshout(string) {
-  console.log("shout")
+function longshout(string.toUpperCase) {
+  console.log("Hello")
 }
